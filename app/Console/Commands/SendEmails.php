6e72d33807adcb7fba884673915fb4d4cd26623c
Pascual Pérez <pereznango@gmail.com>
2025-09-14 20:29:00 -0600

    Add Console Command

---
+++ app/Console/Commands/SendEmails.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+
+class SendEmails extends Command implements PromptsForMissingInput
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-emails {option : Any valid option} {--max=10 : Maximum of items}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send pending emails';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $arguments = $this->arguments();
+
+        \Log::info(static::class, $arguments);
+
+        $this->info('Sent emails successfully.');
+        $this->fail('Sent emails failed.');
+    }
+}
